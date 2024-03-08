7130a6a0f062d8f66c0a9fb9f305ea4189243bc1
waseemuddin <waseem.uddin@live.com>
2024-03-08 21:36:49 +0500

    file update

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0df4b2961410d4cff"
+  ami                    = "ami-08f7912c15ca96832"
   instance_type          = "t2.medium"
   key_name               = "eksenty"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
