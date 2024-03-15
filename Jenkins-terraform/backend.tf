15151a6b69e721bfe4a04fa8237cbe293f253f30
waseemuddin <waseem.uddin@live.com>
2024-03-16 00:23:07 +0500

    files updated

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mydevproject-bkt" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "devsecops-project-03/Jenkins-terraform/terraform.tfstate"
     region = "us-east-1"
   }
 }
